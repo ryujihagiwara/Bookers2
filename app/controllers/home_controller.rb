d38ed1db38c96246ac7768946ff15de956d16f53
ryujihagiwara <dragon7103next@icloud.com>
2020-08-03 11:25:04 +0900

    [Add] aboutページ

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+
+  before_action :authenticate_user!, except: [:about]
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/about'
   root 'users#top'
 
   devise_for :users, {
