1d1e61d65804ceb5d418cfa8ee2b52f4c1dbb295
ryujihagiwara <dragon7103next@icloud.com>
2020-08-03 19:05:55 +0900

    [Fix] devise新規登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   	devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
   	devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :email])
   end
+  
 
   def after_sign_in_path_for(resource)
   	user_path(current_user.id)
