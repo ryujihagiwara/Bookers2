0c68e9d83bc1ea8fe9246c57f1ccf46dc4770aad
ryujihagiwara <dragon7103next@icloud.com>
2020-08-04 23:22:17 +0900

    [Fix] ユーザー編集ボタン

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
 	   @book.user_id = current_user.id
 	  if @book.save
 	  	flash[:notice] = "You have created book successfully"
-	  	redirect_to book_path(@book.id)
+	  	redirect_to book_path(@book)
 	  else
 	  	 @user = current_user
 	     @books = Book.all
